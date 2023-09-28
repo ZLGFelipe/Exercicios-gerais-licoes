f72b35a71455b6dd3f960ea4a730fa8d36eb46a5
Felipe <142358423+ZLGFelipe@users.noreply.github.com>
2023-09-28 17:58:53 +0000

    Correção dos exercicios

--- Lista de Vetores e Matrizes/exercicio_3__Iniciante.py
+++ Lista de Vetores e Matrizes/exercicio_3__Iniciante.py
@@ -1,3 +1,39 @@
 '''
 Crie uma função que receba uma matriz e retorne a transposta dela.
 '''
+
+def transpor(matriz: list) -> list:
+    transposta = []
+
+    for _ in range(len(matriz[0])):
+        transposta.append([0 for _ in range(len(matriz))])
+   
+    for i, l in enumerate(matriz):  
+        for j, v in enumerate(l): 
+            transposta[j][i] = v
+
+    return transposta
+
+def transpor_chatGpt(matriz):
+    num_linhas = len(matriz)
+    num_colunas = len(matriz[0])
+
+    transposta = [[0 for _ in range(num_linhas)] for _ in range(num_colunas)]
+
+    for i in range(num_linhas):
+        for j in range(num_colunas):
+            transposta[j][i] = matriz[i][j]
+
+    return transposta
+
+
+
+matriz = [
+    [1, 7, 5],
+    [8, 2, 1],
+    [4, 9, 0],
+    [2, 3, 9]
+]
+
+t = transpor(matriz)
+print(t)
--- Lista de Vetores e Matrizes/exercicio_4__Iniciante.py
+++ Lista de Vetores e Matrizes/exercicio_4__Iniciante.py
@@ -1,3 +1,21 @@
 '''
 Crie uma função que verifique se uma matriz é simétrica.
 '''
+
+def e_simetrica(matriz: list) -> bool:
+    tam = len(matriz)
+    for i in range(tam):
+        for j in range(tam):
+            if i != j and matriz[i][j] != matriz[j][i]:
+                return False
+    
+    return True
+
+
+matriz = [
+    [1, 2, 4],
+    [2, 5, 6],
+    [4, 6, 7],
+]
+
+print(e_simetrica(matriz))
