7b443dab141901f97149b066b8e4ca28fece6cab
Felipe <142358423+ZLGFelipe@users.noreply.github.com>
2023-10-05 20:15:02 -0300

    Update como_funiona_o_enumerate.py

--- Lições de aula/como_funiona_o_enumerate.py
+++ Lições de aula/como_funiona_o_enumerate.py
@@ -4,7 +4,7 @@ def como_funciona_o_enumerate(lista: list):
         print(f'{contador} - {item}')
         contador += 1'''
     
-    for item, indice in enumerate(lista):
+    for indice, item in enumerate(lista):
         print(f'{indice+1} - {item}')
 
 frutas = [
